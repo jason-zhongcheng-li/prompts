222eb092b66a6f46def348d26c57ac274e6b7221
ZHONGCHENG LI <zhongcheng@192-168-1-112.tpgi.com.au>
2023-11-04 22:53:53 +1100

    feat: implemented getting all posts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+'use client';
 
-const PromptCard = () => {
-   return <div></div>;
+import { useState } from 'react';
+import Image from '@node_modules/next/image';
+import { useSession } from '@node_modules/next-auth/react';
+import { useRouter, usePathname } from '@node_modules/next/navigation';
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+   const [copied, setCopied] = useState('');
+
+   const onCopyClicked = (prompt) => {
+      setCopied(prompt);
+   };
+
+   return (
+      <div className="prompt_card">
+         <div className="flex justify-between items-start gap-5">
+            <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+               <Image
+                  src={post.creator.image}
+                  alt="user_image"
+                  width={40}
+                  height={40}
+                  className="rounded-full object-contain"
+               />
+               <div className="flex flex-col">
+                  <h3 className="font-satoshi font-bold text-gray-900">
+                     {post.creator.username}
+                  </h3>
+                  <p className="font-inter text-sm text-gray-500">
+                     {post.creator.email}
+                  </p>
+               </div>
+            </div>
+            <div
+               className="copy_btn"
+               onClick={() => onCopyClicked(post.prompt)}
+            >
+               <Image
+                  src={
+                     copied === post.prompt
+                        ? '/assets/icons/tick.svg'
+                        : '/assets/icons/copy.svg'
+                  }
+                  width={12}
+                  height={12}
+                  alt="copy_text"
+               />
+            </div>
+         </div>
+         <p className="my-4 font-satoshi text-sm text-gray-700">
+            {post.prompt}
+         </p>
+         <p
+            className="font-inter text-sm blue_gradient cursor-pointer"
+            onClick={() => handleTagClick && handleTagClick(post.tag)}
+         >
+            {post.tag}
+         </p>
+      </div>
+   );
 };
 
 export default PromptCard;
