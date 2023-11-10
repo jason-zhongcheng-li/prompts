23dbd26fa15a893a66cb78df91b5af1d6ff7ebf4
ZHONGCHENG LI <zhongcheng@192-168-1-112.tpgi.com.au>
2023-11-10 23:02:47 +1100

    added avatar and image placeholder

--- components/prompt-card/PromptCardLoader.jsx
+++ components/prompt-card/PromptCardLoader.jsx
@@ -1,39 +1,30 @@
-'use client';
-
-import SkeletonLoader from '@components/loader/SkeletonLoader';
+import AvatarPlaceholder from '../skeleton/avatar-placeholder';
 
 const PromptCardLoader = () => {
    return (
-      <div className="prompt_card">
-         <div className="flex justify-between items-start gap-5">
-            <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-               <SkeletonLoader />
-               <div className="flex flex-col">
-                  <h3 className="font-satoshi font-bold text-gray-900">
-                     <SkeletonLoader />
-                  </h3>
-                  <div className="font-inter text-sm text-gray-500">
-                     <SkeletonLoader />
+      <div className="prompt_card ">
+         <div className="animate-pulse">
+            <div className="flex justify-between items-start gap-5">
+               <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+                  <AvatarPlaceholder />
+                  <div className="flex flex-col">
+                     <h3 className="font-satoshi font-bold text-gray-900">
+                        <div class="h-3.5 bg-gray-200 rounded-full dark:bg-gray-700 w-24 mb-4  "></div>
+                     </h3>
+                     <div className="font-inter text-sm text-gray-500">
+                        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-1"></div>
+                     </div>
                   </div>
                </div>
+               <div className="copy_btn"></div>
             </div>
-            <div className="copy_btn">
-               <SkeletonLoader />
+            <div className="my-4 font-satoshi text-sm text-gray-700">
+               <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-72 mb-4  "></div>
+               <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-72 mb-4  "></div>
+               <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-72 mb-4  "></div>
             </div>
-         </div>
-         <div className="my-4 font-satoshi text-sm text-gray-700">
-            <SkeletonLoader />
-         </div>
-         <div className="font-inter text-sm blue_gradient cursor-pointer">
-            <SkeletonLoader />
-         </div>
-
-         <div className="mt-5 flex-center gap-4 border-t border-gray-200 pt-3">
-            <div className="font-inter text-sm green_gradient cursor-pointer">
-               <SkeletonLoader />
-            </div>
-            <div className="font-inter text-sm orange_gradient cursor-pointer">
-               <SkeletonLoader />
+            <div className="font-inter text-sm blue_gradient cursor-pointer">
+               <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4  "></div>
             </div>
          </div>
       </div>
